a1583c1cac08153c1eaf17bbcf1e4565791c9244
iasmim <iasmimstuff@gmail.com>
2022-06-07 21:36:52 -0300

    create: post providers, function get all posts

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import Provider from './providers';
 
-const container = document.getElementById("root") as HTMLElement;
+const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 
 root.render(
   <BrowserRouter>
+    <Provider>
       <App />
+    </Provider>
   </BrowserRouter>
 );
